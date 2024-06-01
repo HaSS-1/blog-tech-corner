66db4537b085553c4d4fea06c17328e8ca9cad85
has-1 <tajiri.hassan@gmail.com>
2024-06-01 22:47:39 +0200

    modify packageJson & viteconfig to make server live over local ntwrk

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Button } from "flowbite-react"
 
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <div><h1>SignIn</h1>
+    <Button>Click me</Button>
+    </div>
+    
+      
+    
   )
 }
