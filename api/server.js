8739b7f350fc91968acba5d5a1f5270d9d3db814
has-1 <tajiri.hassan@gmail.com>
2024-06-02 16:08:33 +0200

    setup  and connect DB :building_construction:

--- api/server.js
+++ api/server.js
@@ -1,5 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
 
+dotenv.config()
+mongoose.connect(
+    process.env.MONGO
+).then(
+    () => {console.log('DB connected ')}
+).catch(error => {
+    console.log(error)
+})
 
 const app = express()
 const PORT = 5173
@@ -8,3 +18,4 @@ const HOST = '0.0.0.0' //listen on all network interfaces
 app.listen(PORT, HOST, () => {
     console.log(`Server is now running on --> http://${HOST}:${PORT}`)
 })
+
