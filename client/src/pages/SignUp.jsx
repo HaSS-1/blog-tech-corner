8fdd41c0fb88be4998d464d2915fb0d6b9ac908e
has-1 <tajiri.hassan@gmail.com>
2024-06-04 13:55:49 +0200

    add functionality to the sign up form :triangular_flag_on_post:

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,26 @@
 import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    
+    setFormData({...formData, [e.target.id]: e.target.value});
+    console.log(formData);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/auth/signup', {
+      method: 'POST',
+      headers : {'Content-Type': 'application/json'},
+      body: JSON.stringify(formData)
+    });
+    const data = await res.json();
+    }
+    catch (error) {}
+  }
   return (
     <div className="min-h-screen mt-20 space-x-5">
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-[3.75rem]'>
@@ -24,18 +42,18 @@ export default function SignUp() {
         </div>
         {/* right */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Your Username"></Label>
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput type="text" placeholder="Username" id="username" onChange={handleChange}/>
             </div>
             <div>
               <Label value="Your Email"></Label>
-              <TextInput type="email" placeholder="your@email.com" id="email" />
+              <TextInput type="email" placeholder="your@email.com" id="email" onChange={handleChange}/>
             </div>
             <div>
               <Label value="Your Username"></Label>
-              <TextInput type="text" placeholder="Password" id="password" />
+              <TextInput type="password" placeholder="Password" id="password" onChange={handleChange}/>
             </div>
             <Button gradientDuoTone='purpleToPink' type="submit" >Sign Up</Button>
           </form>
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5173,
-  }
+    proxy: {
+      '/api':{
+        target: 'http://localhost:3000',
+        secure: false,
+    },
+  },
+  host: '0.0.0.0',
+port: 5173,
+},
+plugins: [react()],
+
 })
