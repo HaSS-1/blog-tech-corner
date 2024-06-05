4f361888a3a056b8972d5b90dee827ca233fbaf8
has-1 <tajiri.hassan@gmail.com>
2024-06-05 17:31:08 +0200

    add signing Functionality :construction:

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -27,3 +27,24 @@ export const signup = async (req, res, next) => {
         next(error);
     }
     };
+
+    export const signin = async(req, res, next) => {
+        const { email, password } = req.body;
+
+        if (!email || !password || email === '' || password === '') {
+            next(errorHandler(400, 'All fields are required'));
+        }
+        try {
+            const validUser = await User.findOne({email});
+            if (!validUser) {
+                next(errorHandler(404, 'Passwor or Username is not valid '))
+            }
+
+            const validPassword = bcryptjs.compareSync(password, validUser.password);
+            if (!validPassword) {
+                next(errorHandler(400, 'Passwor or Username is not valid '))
+            }
+        }catch(error) {
+
+        }
+    }
