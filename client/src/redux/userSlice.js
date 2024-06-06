a74ad497bdc125f0cd9ef3b86bcda1f6ffdbb33b
has-1 <tajiri.hassan@gmail.com>
2024-06-06 18:09:09 +0200

    adding redux toolkit in progress

---
+++ client/src/redux/userSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    currentUser: null,
+    errot: null,
+    loading: false
+}
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+        reducer: {
+            signInStart: (state) => {
+                state.loading= true;
+                state.error= null;
+            },
+            signinSuccess: (state, action) => {
+                state.currentUser = action.payload;
+                state.loading = false;
+                state.error = null;
+            },
+            signInFailure: (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            }
+        
+    }
+});
+
+export const { signInStart, signinSuccess, signInFailure } = userSlice.actions;
