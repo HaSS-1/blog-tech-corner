54d65ce583274b7d262e90f248ab72952490a6ff
has-1 <tajiri.hassan@gmail.com>
2024-06-07 14:56:34 +0200

    add reducer fonctionality

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { buildGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
+import persistStore from 'redux-persist/es/persistStore';
 
-
- export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+const rootReducer = combineReducers({
+  user: userReducer,
 });
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistReducer = persistReducers(persistConfig, rootReducer);
+
+ export const store = configureStore({
+  reducer: persistReducer,
+  middleware: (buildGetDefaultMiddleware) =>
+    buildGetDefaultMiddleware({serializableCheck: false}),
+});
+
+export const persistor = persistStore(store)
