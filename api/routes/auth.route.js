f6f146778b242e8de0f3bbf5f21b734c49318949
has-1 <tajiri.hassan@gmail.com>
2024-06-07 23:55:21 +0200

    Add google OAuth Route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,6 +4,7 @@ import { signin, signup } from '../controller/auth.controller.js'
 const router = express.Router()
 
 router.post('/signup', signup);
-router.post('/signin', signin)
+router.post('/signin', signin);
+router.post('/google')
 
 export default router
