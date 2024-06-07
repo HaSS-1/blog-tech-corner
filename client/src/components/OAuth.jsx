2282336f30810ccaa488583775cdeb927fabccf1
has-1 <tajiri.hassan@gmail.com>
2024-06-07 23:11:08 +0200

    Add google OAuth functionality for signIn and signUp :sparkles:

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Button } from 'flowbite-react'
+import { AiFillGoogleCircle } from 'react-icons/ai'
+import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth'
+import { app } from '../firebase'
 
 export default function OAuth() {
+    const auth = getAuth(app)
+    const handleGoogleClick = async () => {
+        const provider = new GoogleAuthProvider()
+        provider.setCustomParameters({prompt: 'select_account'})
+        try {
+            const resultsFromGoogle = await signInWithPopup(auth, provider)
+            console.log(resultsFromGoogle)
+        }catch{
+            console.error();
+            console.log(error)
+        }
+    }
   return (
-    <div>OAuth</div>
+    
+    <Button type='button'outline gradientDuoTone='purpleToBlue' onClick={
+        handleGoogleClick}>
+
+    <AiFillGoogleCircle className='w-6 h-6 mr-2' />
+    Continue with Google
+    </Button>
   )
 }
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "tech-corner-cedb4.firebaseapp.com",
   projectId: "tech-corner-cedb4",
   storageBucket: "tech-corner-cedb4.appspot.com",
@@ -14,4 +14,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
