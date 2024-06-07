e34fb5bedc7a83d68cc367b208350c67ab41e8ac
has-1 <tajiri.hassan@gmail.com>
2024-06-08 00:06:25 +0200

    Completed OAuth funcionality

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
-import { Button } from 'flowbite-react'
-import { AiFillGoogleCircle } from 'react-icons/ai'
-import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth'
-import { app } from '../firebase'
+import React from 'react';
+import { Button } from 'flowbite-react';
+import { AiFillGoogleCircle } from 'react-icons/ai';
+import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
+import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice'
+import { useNavigate } from 'react-router-dom';
+
 
 export default function OAuth() {
-    const auth = getAuth(app)
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const auth = getAuth(app);
+
     const handleGoogleClick = async () => {
         const provider = new GoogleAuthProvider()
         provider.setCustomParameters({prompt: 'select_account'})
         try {
             const resultsFromGoogle = await signInWithPopup(auth, provider)
-            console.log(resultsFromGoogle)
+            const res = await fetch('api/auth/google', {
+                method: 'POST',
+                headers: { 'content-Type': 'application/json' },
+                body: JSON.stringify({
+                    name: resultsFromGoogle.user.displayName,
+                    email: resultsFromGoogle.user.email,
+                    googlePhotoUrl: resultsFromGoogle.user.photoURL
+                })
+            })
+            const data = await res.json()
+            if (res.ok) {
+                dispatch(signInSuccess(data))
+                navigate('/')
+            }
         }catch{
             console.error();
             console.log(error)
