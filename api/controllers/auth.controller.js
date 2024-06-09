96eaa8c158334165091d09c8c01ee8c8586bd56d
has-1 <tajiri.hassan@gmail.com>
2024-06-09 14:58:51 +0200

    fix some typos :pencil2:

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -62,14 +62,14 @@ export const signup = async (req, res, next) => {
         try {
             const user = await User.findOne({email});
             if (user) {
-                const token = jwt.sign({id: user.id}, process.env.JWT_SECRET);
+                const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
                 const {password, ...rest} = user._doc;
                 res.status(200).cookie('access_token', token, {
                     httpOnly: true,
                 }).json(rest);
             } else {
                 const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
-                const hashedPassword = bcryptjs(generatedPassword, 10);
+                const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
                 const newUser = new User({
                     username: name.toLowerCase().split(' ').join('') + Math.random().toString(9).slice(-4) ,
                     email,
